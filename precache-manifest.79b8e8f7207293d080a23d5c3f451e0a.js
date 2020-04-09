bd92afcab82025ede674234a6a6074b70987c8c7
Mauricio Cartagena <mc973220@gmail.com>
2020-04-09 01:53:29 -0400

    Updates

---
+++ precache-manifest.79b8e8f7207293d080a23d5c3f451e0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d44ddf3d81f445fad1ace71631c4994c",
+    "url": "/my-perfil/index.html"
+  },
+  {
+    "revision": "9fd6050873fb22647665",
+    "url": "/my-perfil/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2084b313506543ab806e",
+    "url": "/my-perfil/static/js/2.a8d78088.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-perfil/static/js/2.a8d78088.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fd6050873fb22647665",
+    "url": "/my-perfil/static/js/main.35b6575a.chunk.js"
+  },
+  {
+    "revision": "0876cf0a212e0f077ede",
+    "url": "/my-perfil/static/js/runtime-main.52c2ff80.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-perfil/static/media/logo.5d5d9eef.svg"
+  }
+]);
